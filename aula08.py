8881bd4cd134fe69f62fdc44f533f9e57c8c0496
augusto <marquinaugustocruz@gmail.com>
2025-03-04 06:17:04 -0300

    a.2.0

---
+++ aula08.py
@@ -0,0 +1,25 @@
+import math
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz e {} é igual a {}.'.format(num, math.floor(raiz)))
+
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz e {} é igual a {}.'.format(num, math.ceil(raiz)))
+
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz e {} é igual a {}.'.format(num, raiz))
+
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz e {} é igual a {:.2f}.'.format(num, raiz))
+
+#from math import sqrt, floor
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz e {} é igual a {:.2f}.'.format(num, raiz))
+
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz e {} é igual a {}.'.format(num, floor (raiz)))
---
+++ ex020.py
@@ -0,0 +1,6 @@
+import 
+n1 = str(input('Primeiro aluno: '))
+n2 = str(input('Segundo aluno: '))
+n3 = str(input('Terceiro aluno: '))
+n4 = str(input('Quarto aluno: '))
+
